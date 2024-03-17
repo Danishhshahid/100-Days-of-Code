ed94e3b884e8229d8394f46a57d830d4d38632ba
Danish Shahid <Danishhshahid@gmail.com>
2024-03-18 04:55:27 +0500

    sixth commit

---
+++ Day 6/Question 1/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//Adding more guest:
+let Guest = ["sijawal", "Saif", "shoukat"];
+console.log(`Old Guest List: ${Guest}\n`);
+Guest.unshift("Danish");
+Guest.splice(1, 0, "Younis");
+Guest.push("Mehran");
+console.log(`New Guest List:\t ${Guest}`);
+//Printing the Message:
+Guest.forEach(Guest => {
+    console.log(`Hello Dear ${Guest}, You are invited to the party`);
+});
---
+++ Day 6/Question 1/main.ts
@@ -0,0 +1,14 @@
+//Adding more guest:
+
+let Guest: string[] = ["sijawal", "Saif", "shoukat"];
+console.log(`Old Guest List: ${Guest}\n`);
+
+Guest.unshift("Danish");
+Guest.splice(1, 0, "Younis");
+Guest.push("Mehran");
+console.log(`New Guest List:\t ${Guest}`);
+
+//Printing the Message:
+Guest.forEach((Guest) => {
+  console.log(`Hello Dear ${Guest}, You are invited to the party`);
+});
