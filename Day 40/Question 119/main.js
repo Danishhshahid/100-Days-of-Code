bde51f3093fc2305d9db2a91a22ce2585dfa7bad
Danish Shahid <Danishhshahid@gmail.com>
2024-04-27 08:04:26 +0500

    40th commit

---
+++ Day 40/Question 119/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Create a while loop that logs "Hello, World!" 5 times.
+let number = 0;
+while (number <= 5) {
+    console.log(` ${number} ,Hello World`);
+    number++;
+}
---
+++ Day 40/Question 120/main.ts
@@ -0,0 +1,14 @@
+//Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+let FavrteMovies : string[]=[
+    "The Lucy",
+    "Kingsman",
+    "Intesteller",
+    "Titanic"
+];
+
+for (const movies of FavrteMovies) {
+    console.log(movies);
+    
+    
+}
