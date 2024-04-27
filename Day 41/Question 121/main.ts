12b101a2d04b0ca4b740e9c9ddb9776a2ce54761
Danish Shahid <Danishhshahid@gmail.com>
2024-04-27 08:29:34 +0500

    41th commit

---
+++ Day 41/Question 121/main.ts
@@ -0,0 +1,9 @@
+//Implement a for loop that counts from 1 to 10 but skips number 5 using the continue statement.
+
+for (let i =1; i <=5; i++){
+    if(i === 2){
+        continue
+    }
+    console.log(i);
+    
+}
---
+++ Day 41/Question 123/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Create a loop that iterates through a string and stops when it finds the first vowel.
+function Loguntilvowel(str) {
+    const vowels = "aeiouAEIOU";
+    for (const char of str) {
+        if (vowels.includes(char)) {
+            console.log(`First Vowel Found: ${char}`);
+            break;
+        }
+    }
+}
+Loguntilvowel("Danish");
+Loguntilvowel("Sijawal");
+Loguntilvowel("Shoukat");
