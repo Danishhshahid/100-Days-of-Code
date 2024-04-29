b68e154d6c06010301832f67b554ed7ed30263d8
Danish Shahid <Danishhshahid@gmail.com>
2024-04-29 18:55:09 +0500

    46th commit

---
+++ Day 46/Question 137/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Implement a try-catch block to handle potential errors in a block of code.
+try {
+    let myName = "danish";
+    console.log(myName);
+}
+catch (error) {
+    console.log(`Error in myName Variable`);
+}
+finally {
+    console.log(`Finally Free`);
+}
---
+++ Day 46/Question 137/main.ts
@@ -0,0 +1,10 @@
+// Implement a try-catch block to handle potential errors in a block of code.
+
+try {
+    let myName = "danish"
+    console.log(myName);
+} catch (error) {
+    console.log(`Error in myName Variable`);
+    
+    
+}
