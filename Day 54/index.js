d98725b61f4aa043d3e2876e4c19ddc436d3e0b3
Danish Shahid <Danishhshahid@gmail.com>
2024-06-07 16:47:59 +0500

    54th commit

---
+++ Day 54/index.js
@@ -0,0 +1,14 @@
+// Day 54 of 100 days coding challenge
+
+//hello world
+
+let message = "hello world"
+alert(message)
+
+
+// variables
+let admin;
+let name1 = "john"
+
+admin = name1
+alert(admin)
